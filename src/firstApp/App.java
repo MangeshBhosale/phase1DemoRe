af4c06ac0eedd4e99cb86dc66dd3e2199889fc83
Mangesh Bhosale <mangesh@192.168.2.203>
2020-12-11 11:16:02 +0530

    phase1DemoRe

---
+++ src/firstApp/App.java
@@ -0,0 +1,17 @@
+package firstApp;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Check the app");
+
+		InputStream in;
+		BufferedReader bufferreader = new BufferedReader(new InputStreamReader(in));
+	}
+
+}
